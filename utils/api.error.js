e7f77de7fb4e00d80b1632fa836ee2366d94dc3a
Jerin <jerinjohnson31788@gmail.com>
2026-07-08 12:27:38 +0530

    refactor: formated code using prettier

--- utils/api.error.js
+++ utils/api.error.js
@@ -1,10 +1,10 @@
 class ApiError extends Error {
-  constructor(statusCode, message) {
-    super(message);
+    constructor(statusCode, message) {
+        super(message);
 
-    this.success = false;
-    this.statusCode = statusCode;
-  }
+        this.success = false;
+        this.statusCode = statusCode;
+    }
 }
 
 export default ApiError;
